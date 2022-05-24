dbd5eb343da9a06be317b78b0c3285143427faff
Alex Beggs <abeggs@spotify.com>
2022-05-24 15:26:29 -0400

    Fix lint issues

--- examples/Counter/build.gradle.kts
+++ examples/Counter/build.gradle.kts
@@ -49,16 +49,16 @@ dependencies {
     implementation(project(":mobius-timetravel-server-http"))
     implementation(project(":mobius-timetravel-core"))
 
-    implementation("com.google.android.material:material:1.5.0")
+    implementation("com.google.android.material:material:1.6.0")
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.appcompat:appcompat:1.4.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     // LiveData
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.1")
     // ViewModel
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
-    implementation("androidx.fragment:fragment-ktx:1.3.6")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.1")
+    implementation("androidx.fragment:fragment-ktx:1.4.1")
 
     testImplementation("com.spotify.mobius:mobius-test:1.5.8")
 
