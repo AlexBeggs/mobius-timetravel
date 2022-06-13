ba15c669881657f43dc0fbe1b01821ca570e64fb
Alex Beggs <abeggs@spotify.com>
2022-06-13 11:34:06 -0400

    Bump Kotlin to 1.7.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,9 @@ buildscript {
 }
 
 plugins {
-    kotlin("jvm") version "1.6.21" apply false
+    kotlin("jvm") version "1.7.0" apply false
     id("com.android.application") version "7.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.6.20" apply false
+    id("org.jetbrains.kotlin.android") version "1.7.0" apply false
 
     id("com.diffplug.spotless") version "6.4.2"
 }
